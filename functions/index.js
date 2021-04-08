12e1312c137f48a8526c25fc176cd01d20af960e
Kwabena-Kobiri <56099212+Kwabena-Kobiri@users.noreply.github.com>
2021-04-08 12:18:35 +0000

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")('sk_test_51HRVFoDabjo4E8a5TosDN5PZdzQjNRGipW6YWlixebdGZeOWXbTco7Xpfy4NDfv094I3DxNENDmc0GlptkQLoEJ2006Ila58ZD')
+const stripe = require("Stripe API Key goes here")
 
 // Setting Up An API
 
